8e187d722682bea5a08fbda21cb1fbea497edfa5
Maycon Belfort <maycon@maxinst.com>
2021-01-05 10:01:02 -0300

    feat: :construction: create an applicaiton bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {
+  AppBar, Grid, IconButton, Toolbar, Typography,
+} from '@material-ui/core';
+import { AccessAlarm, Settings } from '@material-ui/icons';
+import FlexContainer from 'components/FlexContainer';
+
+const App: React.FC = () => (
+  <FlexContainer>
+    <AppBar position="relative">
+      <Toolbar variant="dense">
+        <Grid container direction="row" spacing={3} justify="space-between">
+          <Grid item>
+            <AccessAlarm />
+          </Grid>
+          <Grid item>
+            <Typography variant="h6">Pomodoro App - MasterClass</Typography>
+          </Grid>
+          <Grid item>
+            <IconButton edge="end">
+              <Settings />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+    Hello Mundo!!!
+  </FlexContainer>
+);
 
 export default App;
