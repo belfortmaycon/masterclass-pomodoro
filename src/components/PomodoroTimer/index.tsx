e1c326726812f331bc28cab8e6ccff209cf7adef
Maycon Belfort <maycon@maxinst.com>
2021-01-06 11:52:10 -0300

    feat: :construction: create a conditional style for card header

--- src/components/PomodoroTimer/index.tsx
+++ src/components/PomodoroTimer/index.tsx
@@ -4,12 +4,18 @@ import { Card, CardContent, CardHeader } from '@material-ui/core';
 import FlexContainer from 'components/FlexContainer';
 import { Timer } from 'components/Timer';
 
+import { IPomodoroStyles } from './interfaces';
+import { PomodoroTimerStyle } from './styles';
+
 export default function PomodoroTimer(): JSX.Element {
+  const styledProps: IPomodoroStyles = { isWorking: true };
+  const classes = PomodoroTimerStyle(styledProps);
+
   return (
     <FlexContainer>
       <Card>
-        <CardHeader title="Você está: Trabalhando" />
-        <CardContent>
+        <CardHeader className={classes.title} title="Você está: Trabalhando" />
+        <CardContent className={classes.content}>
           <Timer mainTime={2000} />
         </CardContent>
       </Card>
---
+++ src/components/PomodoroTimer/interfaces.ts
@@ -0,0 +1,3 @@
+export interface IPomodoroStyles {
+  isWorking: boolean
+}
