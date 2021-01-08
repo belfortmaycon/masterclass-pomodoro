3610fe63f27badd295bbfbff917bfcc3d893db36
Maycon Belfort <maycon@maxinst.com>
2021-01-08 17:38:43 -0300

    feat: :sparkles: configure pomodoro timer params

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import PomodoroTimer from 'components/PomodoroTimer';
+import { StoreState } from 'store/modules';
+import { minutesToSecond } from 'utils/minutes-to-second';
 
-const Home: React.FC = () => (
-  <PomodoroTimer
-    pomodoroTime={10}
-    shortRestTime={3}
-    longRestTime={5}
-    cycles={2}
-  />
-);
+const Home: React.FC = () => {
+  const {
+    pomodoroTime,
+    shortRestTime,
+    longRestTime,
+    cycles,
+  } = useSelector((state: StoreState) => state.configuration);
+  return (
+    <PomodoroTimer
+      pomodoroTime={minutesToSecond(pomodoroTime)}
+      shortRestTime={minutesToSecond(shortRestTime)}
+      longRestTime={minutesToSecond(longRestTime)}
+      cycles={minutesToSecond(cycles)}
+    />
+  );
+};
 
 export default Home;
